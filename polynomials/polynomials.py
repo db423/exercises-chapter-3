d6a8cd7411ecaefd5ac464d74b755d49bb341bbd
Danyal Butt <danyal.butt23@imperial.ac.uk>
2024-10-16 16:48:51 +0100

    mult and rmult

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -65,3 +65,21 @@ class Polynomial:
         x = self-other
         return Polynomial(tuple([-1*i for i in x.coefficients]))
     
+    def __mul__(self,other):
+        if isinstance(other,Number):
+            coefs = tuple(other*i for i in self.coefficients)
+            return Polynomial(coefs)
+        elif isinstance(other,Polynomial):
+            coefs=tuple([0 for i in range(len(self.coefficients)+ len(other.coefficients))])
+            coefs_ls = list(coefs)
+            for i in range(len(self.coefficients)):
+                for j in range(len(other.coefficients)):
+                    coefs_ls[i+j] += self.coefficients[i] * other.coefficients[j]
+            coefs_ls[0] = self.coefficients[0] * other.coefficients[0]
+            coefs = tuple(coefs_ls)
+            return Polynomial(coefs)
+    
+    def __rmul__(self,other):
+        return self*other
+    
+    
